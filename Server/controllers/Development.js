300134c897c3394da3bc2c32016232a7a32dc31d
Paul Schütz <paulschuetz.mail@gmail>
2018-07-28 18:01:03 +0200

    node.js initial server

---
+++ Server/controllers/Development.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Development = require('../service/DevelopmentService');
+
+module.exports.createUser = function createUser (req, res, next) {
+  var user = req.swagger.params['user'].value;
+  Development.createUser(user)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getGames = function getGames (req, res, next) {
+  var offset = req.swagger.params['offset'].value;
+  var limit = req.swagger.params['limit'].value;
+  Development.getGames(offset,limit)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getLobby = function getLobby (req, res, next) {
+  var id = req.swagger.params['id'].value;
+  Development.getLobby(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getUsers = function getUsers (req, res, next) {
+  var offset = req.swagger.params['offset'].value;
+  var limit = req.swagger.params['limit'].value;
+  Development.getUsers(offset,limit)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.joinLobby = function joinLobby (req, res, next) {
+  var id = req.swagger.params['id'].value;
+  var user = req.swagger.params['user'].value;
+  Development.joinLobby(id,user)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ Server/service/DevelopmentService.js
@@ -0,0 +1,181 @@
+'use strict';
+var mongoose = require('mongoose')
+const path = require("path");
+var swaggerMongoose = require('swagger-mongoose');
+var fs = require('fs')
+
+console.log("TEST")
+
+var swagger = fs.readFileSync('swagger.json', "utf8");
+var User = swaggerMongoose.compile(swagger).models.User;
+var pablo = new User({name: 'pablo', password: 'secret-shit'})
+pablo.save();
+
+/**
+ * create new user
+ *
+ * user User the user to create (optional)
+ * no response value expected for this operation
+ **/
+exports.createUser = function(user) {
+  return new Promise(function(resolve, reject) {
+    var newUser = new User(user)
+    newUser.save().then(function(user){
+      resolve(user)
+    }).catch(function(err){
+      reject(err)
+    })
+  })
+}
+
+/**
+ * get collection of games
+ *
+ * offset Integer The number of items to skip before starting to collect the result (optional)
+ * limit Integer The number of items to return (optional)
+ * returns List
+ **/
+exports.getGames = function(offset, limit) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [
+      {
+        "gameType": {
+          "numberOfPlayersAllowed": 5,
+          "name": "Summoners Rift"
+        },
+        "name": "Fortnite"
+      }, {
+        "gameType": {
+          "numberOfPlayersAllowed": 5,
+          "name": "Summoners Rift"
+        },
+        "name": "Fortnite"
+      }
+    ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+/**
+ * get a lobby
+ * get a lobby
+ *
+ * id Integer the lobby id
+ * returns Lobby
+ **/
+exports.getLobby = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+      "game": {
+        "gameType": {
+          "numberOfPlayersAllowed": 5,
+          "name": "Summoners Rift"
+        },
+        "name": "Fortnite"
+      },
+      "lobbyMembers": [
+        {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }, {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }
+      ],
+      "invitedUsers": [
+        {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }, {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }
+      ]
+    };
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+/**
+ * get list of users
+ * get collection of users
+ *
+ * offset Integer The number of items to skip before starting to collect the result (optional)
+ * limit Integer The number of items to return (optional)
+ * returns List
+ **/
+exports.getUsers = function(offset, limit) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [
+      {
+        "password": "very-secret",
+        "name": "808Frittenbude"
+      }, {
+        "password": "very-secret",
+        "name": "808Frittenbude"
+      }
+    ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+/**
+ * add a user to a lobby
+ * add a user-reference to a lobby
+ *
+ * id Integer id of the lobby the user joins
+ * user User userId of the user joining the group (optional)
+ * returns Lobby
+ **/
+exports.joinLobby = function(id, user) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+      "game": {
+        "gameType": {
+          "numberOfPlayersAllowed": 5,
+          "name": "Summoners Rift"
+        },
+        "name": "Fortnite"
+      },
+      "lobbyMembers": [
+        {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }, {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }
+      ],
+      "invitedUsers": [
+        {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }, {
+          "password": "very-secret",
+          "name": "808Frittenbude"
+        }
+      ]
+    };
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}

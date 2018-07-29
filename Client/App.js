0719326483c34546efa424ab1274ce3693cbf514
Lukas Reich <lukasreich@online.de>
2018-07-29 20:25:36 +0200

    Background Image
    + Bild

--- Client/App.js
+++ Client/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, Text, ImageBackground } from 'react-native';
 
 export default class FlexDimensionsBasics extends Component {
   render() {
+
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-      </View>
+      <ImageBackground source={require('./Assets/KluftcallBackground.png')} style={{width: '100%', height: '100%'}}>
+        <Text>Inside</Text>
+      </ImageBackground>
     );
   }
 }
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);

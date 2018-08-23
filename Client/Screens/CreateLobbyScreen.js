cec8d02da839265bfc22f98cf09e9217dc04c1a5
Paul Schütz <paulschuetz.mail@gmail>
2018-08-23 20:49:08 +0200

    delete node_modules

--- Client/Screens/CreateLobbyScreen.js
+++ Client/Screens/CreateLobbyScreen.js
@@ -1,17 +1,55 @@
-import React, {Component} from 'react';
-import {AsyncStorage, View, Text, ImageBackground, Button, TextInput, StyleSheet} from 'react-native';
-import {getUsername, persistUsername} from '../persistence.js'
-import Promise from 'bluebird';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
+import Swiper from 'react-native-swiper';
 
 export default class CreateLobbyScreen extends Component {
-  render() {
-    return (<View style={{
-        justifyContent: "center",
-        flex: 1,
-        alignItems: "center"
-      }}>
-      <Text>CreateLobby Screen</Text>
-    </View>);
+  render(){
+    return (
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
--- README.md
+++
@@ -1,5 +0,0 @@
-# kluft-call
-Lonely playing your favorite game? Invite your friends with one click and game on.
-
-# edit swagger config in browser
-swagger project edit
